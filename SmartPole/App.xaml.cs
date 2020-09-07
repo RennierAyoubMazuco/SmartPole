440d23e32e0e180cf77dbb45dbaedc99446714f2
Matheus <matheusvilar8@gmail.com>
2020-09-07 12:38:00 -0300

    Adicionar arquivos de projeto.

---
+++ SmartPole/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using SmartPole.View;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartPole
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
